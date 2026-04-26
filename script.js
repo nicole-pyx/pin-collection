81b2e32b5e489a362113f82f736b2da9babf2ec6
nicole-pyx <156598801+nicole-pyx@users.noreply.github.com>
2026-04-26 09:47:21 +0000

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 console.log("JS connected!");
 image: "images/kyoto.jpg"
 image: "images/newyork.jpg"
+
+// Create map
+var map = L.map('map').setView([20, 0], 2);
+
+// Add tiles (map background)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// Example pin
+L.marker([35.6762, 139.6503]).addTo(map)
+  .bindPopup(`
+    <h3>Kyoto, Japan</h3>
+    <img src="images/kyoto.jpg" width="200">
+    <p>My first pin from Japan 🇯🇵</p>
+  `);
