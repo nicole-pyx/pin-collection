1445ba1947445ba66097531af2cd1d9b2ada0cbc
nicole-pyx <156598801+nicole-pyx@users.noreply.github.com>
2026-04-26 10:52:29 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 console.log("JS connected!");
-image: "images/kyoto.jpg"
-image: "images/newyork.jpg"
 
 
 // Create map
@@ -20,52 +18,3 @@ L.marker([40.7826, -73.9656]).addTo(map)
     <p>I❤️NY</p>
   `);
 
-let currentImages = [];
-let currentIndex = 0;
-
-function showNext() {
-  currentIndex = (currentIndex + 1) % currentImages.length;
-  document.getElementById("popup-img").src = currentImages[currentIndex];
-}
-
-function showPrev() {
-  currentIndex =
-    (currentIndex - 1 + currentImages.length) % currentImages.length;
-  document.getElementById("popup-img").src = currentImages[currentIndex];
-}
-
-// japan pins
-var pin = {
-  location: "Kyoto, Japan",
-  coords: [35.0116, 135.7681],
-  images: [
-    "images/kyoto.JPG",
-    "images/kyoto2.JPG",
-  ],
-  description: "so cutie"
-};
-
-var pin = {
-  location: "Hokkaido, Japan",
-  coords: [43.2203, 142.8635],
-  images: [
-    "images/hokkaido.JPG",
-    "images/hokkaido2.JPG",
-    "images/hokkaido3.JPG",
-    "images/hokkaido4.JPG"
-  ],
-  description: "hokkaido in the summer"
-};
-
-currentImages = pin.images;
-currentIndex = 0;
-
-L.marker(pin.coords).addTo(map)
-  .bindPopup(`
-    <h3>${pin.location}</h3>
-    <img id="popup-img" src="${pin.images[0]}" width="200">
-    <br><br>
-    <button onclick="showPrev()">⬅️</button>
-    <button onclick="showNext()">➡️</button>
-    <p>${pin.description}</p>
-  `);
