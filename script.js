ae74a80ea0c7a5d466dc1628e9c9ac29db25c225
nicole-pyx <156598801+nicole-pyx@users.noreply.github.com>
2026-04-26 09:51:07 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -14,6 +14,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([35.6762, 139.6503]).addTo(map)
   .bindPopup(`
     <h3>Kyoto, Japan</h3>
-    <img src="images/kyoto.jpg" width="200">
+    <img src="images/kyoto.JPG" width="200">
     <p>My first pin from Japan 🇯🇵</p>
   `);
