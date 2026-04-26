f390673dc0616c453c98bb80d9013f189e588390
nicole-pyx <156598801+nicole-pyx@users.noreply.github.com>
2026-04-26 11:02:54 +0000

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,72 +1,35 @@
 console.log("JS connected!");
 
+
 // Create map
 var map = L.map('map').setView([20, 0], 2);
 
-// Add tiles
+// Add tiles (map background)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// Pin data (IMPORTANT: use ARRAY, not single var)
+
 var pins = [
   {
+    id: "nyc",
     location: "NYC, USA",
     coords: [40.7826, -73.9656],
     images: ["images/newyork.JPG"],
     description: "I ❤️ NY"
   },
   {
+    id: "kyoto",
     location: "Kyoto, Japan",
     coords: [35.0116, 135.7681],
-    images: ["images/kyoto.JPG", "images/kyoto2.JPG"],
-    description: "So cute"
+    images: ["images/kyoto.JPG","images/kyoto2.JPG"],
+    description: "so cutie"
   },
   {
+    id: "hokkaido",
     location: "Hokkaido, Japan",
     coords: [43.2203, 142.8635],
-    images: [
-      "images/hokkaido.JPG",
-      "images/hokkaido2.JPG",
-      "images/hokkaido3.JPG",
-      "images/hokkaido4.JPG"
-    ],
-    description: "Hokkaido in the summer"
+    images: ["images/hokkaido.JPG","images/hokkaido2.JPG", "images/hokkaido3.JPG", "images/hokkaido4.JPG"],
+    description: "hokkaido in the summer"
   }
 ];
-
-// image state per popup
-let imageState = {};
-
-pins.forEach(pin => {
-
-  imageState[pin.location] = {
-    index: 0,
-    images: pin.images
-  };
-
-  L.marker(pin.coords).addTo(map)
-    .bindPopup(`
-      <h3>${pin.location}</h3>
-      <img id="img-${pin.location}" src="${pin.images[0]}" width="200">
-      <br><br>
-      <button onclick="prev('${pin.location}')">⬅️</button>
-      <button onclick="next('${pin.location}')">➡️</button>
-      <p>${pin.description}</p>
-    `);
-
-});
-
-// carousel functions
-function next(city) {
-  let data = imageState[city];
-  data.index = (data.index + 1) % data.images.length;
-  document.getElementById(`img-${city}`).src = data.images[data.index];
-}
-
-function prev(city) {
-  let data = imageState[city];
-  data.index = (data.index - 1 + data.images.length) % data.images.length;
-  document.getElementById(`img-${city}`).src = data.images[data.index];
-}
-
