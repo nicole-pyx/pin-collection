262f26792f8682889165821ee123b3e75164e94d
nicole-pyx <156598801+nicole-pyx@users.noreply.github.com>
2026-04-28 09:03:13 +0000

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
 
-
 // Nyc pin
 L.marker([40.7826, -73.9656]).addTo(map)
   .bindPopup(`
@@ -18,44 +17,3 @@ L.marker([40.7826, -73.9656]).addTo(map)
     <p>I❤️NY</p>
   `);
 
-var pins = [
-  {
-    id: "nyc",
-    location: "NYC, USA",
-    coords: [40.7826, -73.9656],
-    images: ["images/newyork.JPG"],
-    description: "I ❤️ NY"
-  },
-  {
-    id: "kyoto",
-    location: "Kyoto, Japan",
-    coords: [35.0116, 135.7681],
-    images: ["images/kyoto.JPG","images/kyoto2.JPG"],
-    description: "so cutie"
-  },
-  {
-    id: "hokkaido",
-    location: "Hokkaido, Japan",
-    coords: [43.2203, 142.8635],
-    images: ["images/hokkaido.JPG","images/hokkaido2.JPG", "images/hokkaido3.JPG", "images/hokkaido4.JPG"],
-    description: "hokkaido in the summer"
-  }
-];
-
-function openSidebar(pin) {
-  document.getElementById("title").innerText = pin.location;
-  document.getElementById("desc").innerText = pin.description;
-
-  document.getElementById("gallery").innerHTML =
-    pin.images.map(img => `<img src="${img}">`).join("");
-}
-
-pins.forEach(pin => {
-
-  L.marker(pin.coords)
-    .addTo(map)
-    .on('click', () => {
-      openSidebar(pin);
-    });
-
-});
