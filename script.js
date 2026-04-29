653886c504ac25d96db97e5cbbd9795dd7c3e2f2
nicole-pyx <156598801+nicole-pyx@users.noreply.github.com>
2026-04-29 16:31:08 +0000

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -15,21 +15,26 @@ var pins = [
     id: "nyc",
     location: "NYC, USA",
     coords: [40.7826, -73.9656],
-    images: ["images/newyork.JPG"],
+    images: [{src: "images/newyork.JPG"}],
     description: "I ❤️ NY"
   },
   {
     id: "kyoto",
     location: "Kyoto, Japan",
     coords: [35.0116, 135.7681],
-    images: ["images/kyoto.JPG","images/kyoto2.JPG"],
+    images: [{src: "images/kyoto.JPG"},{src: "images/kyoto2.JPG"}],
     description: "so cutie"
   },
   {
     id: "hokkaido",
     location: "Hokkaido, Japan",
     coords: [43.2203, 142.8635],
-    images: ["images/hokkaido.JPG","images/hokkaido2.JPG", "images/hokkaido3.JPG", "images/hokkaido4.JPG"],
+    images: [
+      {src: "images/hokkaido.JPG", text: "Hokkaido bird 🐦"},
+      {src: "images/hokkaido2.JPG", text: "Biei 🎋"},
+      {src: "images/hokkaido3.JPG", text: "Furano 🪻"},
+      {src: "images/hokkaido4.JPG", text: "Noboribetsu ♨️"}
+    ],
     description: "hokkaido in the summer"
   }
 ];
@@ -39,7 +44,12 @@ function openSidebar(pin) {
   document.getElementById("desc").innerText = pin.description;
 
   document.getElementById("gallery").innerHTML =
-    pin.images.map(img => `<img src="${img}">`).join("");
+  pin.images.map(img => `
+    <div class="img-container">
+      <img src="${img.src}">
+      ${img.text ? `<div class="tooltip">${img.text}</div>` : ""}
+    </div>
+    `).join("");
 }
 
 pins.forEach(pin => {
@@ -51,3 +61,5 @@ pins.forEach(pin => {
     });
 
 });
+
+
